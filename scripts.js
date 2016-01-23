778e281c13bd140ff2981f3ae42cdcd1bd5102b5
ejetzer <ejetzer@gmail.com>
2016-01-23 17:01:46 -0500

    Started converting CSS to LESS

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+function hideThing(tag, index, post, censored) {
+	if (tag in censored) {
+		$('#post'.concat(post)).toggleClass('warning');
+	}
+}
+
+function checkTags(tags, post, censored) {
+	tags.forEach(function (tag, index, array) {
+		hideThing(tag, index, post, censored);
+	});
+}
+
+function censor(censored) {
+	alltags.forEach(function (tags, post, map) {
+		checkTags(tags, post, censored);
+	});
+}
+
 $(function () {
-	
+	censor(censored);
 });
